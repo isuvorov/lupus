75b5a945c4eaa907d34980d6327b226c3f44c2b9
Yukioru <obt195@gmail.com>
2016-07-18 19:34:51 +0400

    logic

--- src/LupusApp/components/App/App.jsx
+++ src/LupusApp/components/App/App.jsx
@@ -1,104 +1,249 @@
-import {Component} from 'react'
-import {
-  Navbar, Nav, NavItem, NavDropdown, MenuItem,
-  Grid, Row, Col,
-  ListGroup, ListGroupItem,
-  Tabs, Tab,
-  Panel,
-  Button,
-} from 'react-bootstrap'
-import CloseIcon from 'react-icons/lib/fa/close'
-import SaveIcon from 'react-icons/lib/fa/check'
+import React, { Component } from 'react';
+import { debounce } from 'lodash';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import Tab from 'react-bootstrap/lib/Tab';
+import Well from 'react-bootstrap/lib/Well';
+import Tabs from 'react-bootstrap/lib/Tabs';
+import Panel from 'react-bootstrap/lib/Panel';
+import Table from 'react-bootstrap/lib/Table';
+import Button from 'react-bootstrap/lib/Button';
+import ListGroup from 'react-bootstrap/lib/ListGroup';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import ListGroupItem from 'react-bootstrap/lib/ListGroupItem';
+import CloseIcon from 'react-icons/lib/fa/close';
+import SaveIcon from 'react-icons/lib/fa/check';
+import AddIcon from 'react-icons/lib/fa/plus';
 
-import CSSModules from '~/utils/CSSModules'
+import Header from '../Header';
+import cssModules from '~/utils/CSSModules';
 
-@CSSModules(require('./App.styl'))
+@cssModules(require('./App.styl'))
 export default class App extends Component {
-
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      active: 0
-    }
-    // this.setActive = this.setActive.bind(this)
+      active: 0,
+      project: this.props.projects[0],
+    };
+    this.handleChangeValue = this.handleChangeValue.bind(this)
+
   }
-  setActive(active){
-    return () => {
-      this.setState({active})
-    }
+  setActive = (active) => {
+    const project = this.props.projects[active] || {};
+    this.setState({ active, project });
   }
-  getProject(){
-    return this.props.projects[this.state.active] || {}
+  // handleChangeValue = (object, key) => debounce((e) => {
+  //   console.log('handleChangeValue');
+  //   const project = this.state.project;
+  //   project[object][key] = e.target.value;
+  //   this.setState({ project });
+  // }, 250);
+
+
+
+  handleChangeValue(e) {
+    e.target['kek'] = 'general';
+    console.log(e.target.kek);
+    // const project = this.state.project;
+    // // e.target.data
+    // project[object][key] = e.target.value;
+    // this.setState({ project });
+  }
+  // debounce((e) => {
+  //   console.log('handleChangeValue');
+  //   const project = this.state.project;
+  //   project[object][key] = e.target.value;
+  //   this.setState({ project });
+  // }, 250);
+
+
+  handleChangeKey = (object, key) => (e) => {
+    console.log(e.target);
+    console.log('handleChangeKey');
+    const project = this.state.project;
+    project[object][e.target.value] = project[object][key];
+    delete project[object][key];
+    this.setState({ project });
   }
   render() {
-    return <div styleName='root'>
-      <Navbar inverse>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#">React-Bootstrap</a>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-            <NavItem eventKey={1} href="#">Link</NavItem>
-            <NavItem eventKey={2} href="#">Link</NavItem>
-            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}>Action</MenuItem>
-              <MenuItem eventKey={3.2}>Another action</MenuItem>
-              <MenuItem eventKey={3.3}>Something else here</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey={3.3}>Separated link</MenuItem>
-            </NavDropdown>
-          </Nav>
-          <Nav pullRight>
-            <NavItem eventKey={1} href="#">Link Right</NavItem>
-            <NavItem eventKey={2} href="#">Link Right</NavItem>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-
-      <Grid>
-        <Row>
-          <Col md={4}>
-            <ListGroup>
-              <For each="project" index="index" of={ this.props.projects }>
-                <ListGroupItem
-                  onClick={this.setActive(index)}
-                  active={index === this.state.active}
-                >
-                  {project.name}
-                </ListGroupItem>
-              </For>
-              <Button bsStyle='success'>
-                +
-              </Button>
-           </ListGroup>
-
-          </Col>
-          <Col md={8}>
-
-            <Panel header={<h2>{this.getProject().name} <Button bsSize='small' bsStyle='success' style={{float:'right'}}><SaveIcon /></Button> <Button bsSize='small' bsStyle='danger' style={{float:'right'}}><CloseIcon /></Button> </h2>}>
-
-              Git hook: <a href={'http://panel.s3.mgbeta.ru/projects/refresh?name=' + this.getProject().name}>{'http://panel.s3.mgbeta.ru/projects/refresh?name=' + this.getProject().name}</a>
-
-
-              <Tabs defaultActiveKey={0} id="uncontrolled-tab-example">
-              <Tab eventKey={0} title="Общие данные">
-                Tab 0 content
-               </Tab>
-               <Tab eventKey={1} title="Репозитории">
-                Tab 1 content
-               </Tab>
-               <Tab eventKey={2} title="Файлы">
-                Tab 2 content
-              </Tab>
-             </Tabs>
-            </Panel>
-
-          </Col>
-        </Row>
-      </Grid>
-    </div>
+    const panelHeader = (
+      <h2>{this.state.project.name}
+        <div style={{ marginLeft: 'auto' }}>
+          <Button bsSize="small" bsStyle="danger" style={{ borderRadius: '3px 0 0 3px' }}>
+            <CloseIcon />
+          </Button>
+          <Button bsSize="small" bsStyle="success" style={{ borderRadius: '0 3px 3px 0' }}>
+            <SaveIcon />
+          </Button>
+        </div>
+      </h2>
+    );
+    const projectButton = {
+      borderRadius: '0 0 3px 3px',
+      borderColor: '#ddd',
+      padding: '10px 0',
+    };
+    const { project, active } = this.state;
+    return (
+      <div styleName="root">
+        <Header />
+        <Button onClick={debounce(() => {
+          console.log('CLICK');
+        }, 600)}>CLICK</Button>
+        <Grid>
+          <Row>
+            <Col md={4}>
+              <ListGroup>
+                <For each="prj" index="index" of={ this.props.projects}>
+                  <ListGroupItem
+                    onClick={this.setActive.bind(this, index)}
+                    active={index === active}>
+                    {prj.name}
+                  </ListGroupItem>
+                </For>
+                <Button style={projectButton} block><AddIcon /> Добавить проект</Button>
+              </ListGroup>
+            </Col>
+            <Col md={8}>
+              <Panel header={panelHeader}>
+                <Tabs defaultActiveKey={0} id="uncontrolled-tab-example">
+                  <Tab eventKey={0} title="Информация">
+                    <Table responsive>
+                      <thead>
+                        <tr>
+                          <th>Название</th>
+                          <th>Описание</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>Git hook</td>
+                          <td><a href={`http://panel.s3.mgbeta.ru/projects/refresh?name=${project.name}`}>
+                          {`http://panel.s3.mgbeta.ru/projects/refresh?name=${project.name}`}
+                          </a></td>
+                        </tr>
+                      </tbody>
+                    </Table>
+                    <Button block><AddIcon /> Добавить информацию</Button>
+                  </Tab>
+                  <Tab eventKey={1} title="Общие данные">
+                    <Table responsive>
+                      <thead>
+                        <tr>
+                          <th>Название</th>
+                          <th>Значение</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {Object.keys(project.general).map((key, i) => (
+                          <tr key={`gen-${i}`}>
+                            <td style={{ width: '30%' }}>
+                              <FormControl
+                                type="text"
+                                object="general"
+                                key={key}
+                                placeholder="Введите название"
+                                onChange={this.handleChangeKey}
+                                value={key}
+                              />
+                            </td>
+                            <td style={{ width: '70%' }}>
+                              <FormControl
+                                type="text"
+                                object="general"
+                                key={key}
+                                ref={{ object: 'general', key }}
+                                placeholder="Введите значение"
+                                onChange={this.handleChangeValue}
+                                value={project.general[key]}
+                              />
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </Table>
+                    <Button block><AddIcon /> Добавить данные</Button>
+                  </Tab>
+                  <Tab eventKey={2} title="Репозитории">
+                    <Table responsive>
+                      <thead>
+                        <tr>
+                          <th>Название</th>
+                          <th>Ссылка</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {Object.keys(project.repos).map((key, i) => (
+                          <tr key={`repo-${i}`}>
+                            <td style={{ width: '30%' }}>
+                              <FormControl
+                                type="text"
+                                object="repos"
+                                key={key}
+                                placeholder="Введите название"
+                                onChange={this.handleChangeKey}
+                                value={key}
+                              />
+                            </td>
+                            <td style={{ width: '70%' }}>
+                              <FormControl
+                                type="text"
+                                object="repos"
+                                key={key}
+                                placeholder="Введите ссылку"
+                                onChange={this.handleChangeValue}
+                                value={project.repos[key]}
+                              />
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </Table>
+                    <Button block><AddIcon /> Добавить репозиторий</Button>
+                  </Tab>
+                  <Tab eventKey={3} title="Файлы">
+                    {Object.keys(project.files).map((key, i) => (
+                      <Well styleName="card" bsSize="small" key={`file-${i}`}>
+                        <FormGroup controlId="controlNameFile">
+                          <ControlLabel>Файл</ControlLabel>
+                          <FormControl
+                            type="text"
+                            object="files"
+                            key={key}
+                            placeholder="Введите название файла"
+                            onChange={this.handleChangeKey}
+                            value={key}
+                          />
+                        </FormGroup>
+                        <FormGroup controlId="controlNameFile">
+                          <ControlLabel>Содержимое</ControlLabel>
+                          <pre>
+                            <FormControl
+                              componentClass="textarea"
+                              placeholder="Наполните файл содержимым"
+                              rows={8}
+                              object="files"
+                              key={key}
+                              onChange={this.handleChangeValue}
+                              value={project.files[key]}
+                            />
+                          </pre>
+                        </FormGroup>
+                        <Button bsStyle="danger" block><CloseIcon /> Удалить</Button>
+                      </Well>
+                    ))}
+                    <Button block><AddIcon /> Добавить файл</Button>
+                  </Tab>
+                </Tabs>
+              </Panel>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
   }
 }
---
+++ src/LupusApp/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Navbar from 'react-bootstrap/lib/Navbar';
+import Nav from 'react-bootstrap/lib/Nav';
+import NavItem from 'react-bootstrap/lib/NavItem';
+
+export default function Header() {
+  return (
+    <Navbar staticTop>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#">Lupus</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <NavItem eventKey={1} href="#">Link</NavItem>
+          <NavItem eventKey={2} href="#">Link</NavItem>
+        </Nav>
+        <Nav pullRight>
+          <NavItem eventKey={1} href="#">Link Right</NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
