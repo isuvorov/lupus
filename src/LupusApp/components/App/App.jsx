3df468547bef51e69016f2a06c3f3bebb8345a36
Yukioru <obt195@gmail.com>
2016-07-26 18:49:22 +0400

    add project

--- src/LupusApp/components/App/App.jsx
+++ src/LupusApp/components/App/App.jsx
@@ -5,7 +5,12 @@ import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import Button from 'react-bootstrap/lib/Button';
 import Nav from 'react-bootstrap/lib/Nav';
+import Modal from 'react-bootstrap/lib/Modal';
 import NavItem from 'react-bootstrap/lib/NavItem';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import HelpBlock from 'react-bootstrap/lib/HelpBlock';
 
 import CloseIcon from 'react-icons/lib/fa/close';
 import SaveIcon from 'react-icons/lib/fa/check';
@@ -25,6 +30,8 @@ export default class App extends Component {
     super(props);
     this.state = {
       active: 0,
+      newProject: '',
+      showModal: false,
       projects: [],
       project: {},
     };
@@ -32,9 +39,14 @@ export default class App extends Component {
   componentDidMount() {
     this.getProjects();
   }
+  setActive = (index) => {
+    const active = index;
+    const project = this.state.projects[active] || {};
+    this.setState({ active, project });
+  }
   getProjects = () => {
     let { project } = this.state;
-    fetch('http://y.mgbeta.ru:666/projects', {
+    fetch('http://localhost:3000/api/projects/', {
       method: 'GET',
     })
     .then((res) => res.json())
@@ -44,12 +56,41 @@ export default class App extends Component {
       this.setState({ projects, project });
     });
   }
-  setActive = (index) => {
-    const active = index;
-    const project = this.state.projects[active] || {};
-    this.setState({ active, project });
+  getValidationState = () => {
+    const length = this.state.newProject.length;
+    if (length > 10) return 'success';
+    else if (length > 5) return 'warning';
+    else if (length > 0) return 'error';
   }
-  renderProjectItems() {
+  newProject = () => {
+    const { newProject, projects } = this.state;
+    if (newProject.length > 5) {
+      fetch('http://localhost:3000/api/projects', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: newProject }),
+      })
+      .then((res) => res.json())
+      .then((obj) => obj.data)
+      .then((project) => {
+        projects.push(project);
+        this.setState({ projects, project });
+        this.closeModal();
+      });
+    }
+  }
+  closeModal = () => {
+    this.setState({ showModal: false });
+  }
+  openModal = () => {
+    this.setState({ showModal: true });
+  }
+  handleChange = (e) => {
+    this.setState({ newProject: e.target.value });
+  }
+  renderProjectItems = () => {
     return this.state.projects.map((prj, index) => (
       <NavItem
         onClick={this.setActive.bind(this, index)}
@@ -68,7 +109,7 @@ export default class App extends Component {
             <Col styleName="fullish" className="sidebar" md={2} sm={3}>
               <Nav bsStyle="pills" styleName="inner" stacked>
                 {this.renderProjectItems()}
-                <Button block><AddIcon /> Добавить проект</Button>
+                <Button onClick={this.openModal}><AddIcon /> Добавить проект</Button>
               </Nav>
             </Col>
             <Col md={10} mdOffset={2} sm={9} smOffest={3}>
@@ -78,6 +119,31 @@ export default class App extends Component {
             </Col>
           </Row>
         </Grid>
+        <Modal bsSize="small" show={this.state.showModal} onHide={this.closeModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Создать проект</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <FormGroup
+              controlId="formBasicText"
+              validationState={this.getValidationState()}
+            >
+              <ControlLabel>Название проекта</ControlLabel>
+              <FormControl
+                type="text"
+                value={this.state.newProject}
+                placeholder="Придумайте название"
+                onChange={this.handleChange}
+              />
+              <FormControl.Feedback />
+              <HelpBlock>Не меньше 6 символов</HelpBlock>
+            </FormGroup>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button bsStyle="success" onClick={this.newProject}>Создать</Button>
+            <Button onClick={this.closeModal}>Закрыть</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
