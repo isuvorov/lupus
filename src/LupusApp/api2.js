62e0dfc70a3d61e74bb107c54c1235a9ead1444f
Igor Suvorov <suvorov.igor.90@gmail.com>
2016-07-29 06:02:47 +0400

    changes

---
+++ src/LupusApp/api2.js
@@ -0,0 +1,41 @@
+import asyncRouter from '~/utils/AsyncRouter';
+
+export default (ctx) => {
+  //qwe
+  const api = asyncRouter();
+
+  const Project = ctx.models.Project
+  const e404 = ctx.errors.e404('Project not found')
+
+  api.all('/', (req, res) => res.ok({title: 'api', version: 1}))
+  api.get('/projects', () => Project.find())
+  api.post('/projects', (req) => {
+    const project = new Project(req.body)
+    return project.save()
+  })
+
+  api.get('/projects/byName/:name', (req) => Project.findOne({name: req.params.name}))
+  api.get('/projects/:id', (req) => Project.findOne({_id: req.params.id}))
+  api.put('/projects/:id', async (req) => {
+    return Project.update({_id: req.params.id}, req.body)
+  })
+  api.get('/projects/byName/:name/runTask/:task', (req) => {
+    return Project.findOne({name: req.params.name}).then(project => {
+      if (!project) throw e404
+      return project.runTask(req.params.task)
+    })
+  })
+  api.get('/projects/:id/runTask/:task', (req) => {
+    return Project.findOne({_id: req.params.id}).then(project => {
+      if (!project) throw e404
+      return project.runTask(req.params.task)
+    })
+  })
+  api.get('/projects/byName/:name/syncFs', (req) => {
+    return Project.findOne({name: req.params.name}).then(project => {
+      if (!project) throw e404
+      return project.syncFs()
+    })
+  })
+  return api;
+}
